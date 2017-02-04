5fbc5129b1efad24f7365a34a2347fbb90eb155b
MarkGorbunov <markgorbunov1995@mail.ru>
2017-02-04 16:47:18 +0300

    logger for findNews

--- src/main/java/controller/impl/AddNews.java
+++ src/main/java/controller/impl/AddNews.java
@@ -11,8 +11,8 @@ import service.factory.ServiceFactory;
  * Created by Mark_Harbunou on 2/1/2017.
  */
 public class AddNews implements Command {
-    static final Logger rootLogger = LogManager.getRootLogger();
-    static final Logger addNewsLogger = LogManager.getLogger(AddNews.class);
+    final Logger logger = LogManager.getLogger(AddNews.class.getName());
+
     @Override
     public String execute(String request) {
         String responce = "";
@@ -21,16 +21,14 @@ public class AddNews implements Command {
         NewsService newsService = serviceFactory.getNewsService();
 
         try {
+            if (logger.isDebugEnabled()) {
+                logger.debug("AddNewsLogger in debug");
+            }
             newsService.addNews(request);
             responce = "book added";
-
-            if (rootLogger.isDebugEnabled()) {
-                rootLogger.debug("RootLogger: In debug message");
-                addNewsLogger.debug("UserLogger in debug");
-            }
         } catch (ServiceException e) {
-           addNewsLogger.error("error message: " + e.getMessage());
-            addNewsLogger.fatal("fatal error message: " + e.getMessage());
+            logger.error("error message: " + e.getMessage());
+            logger.fatal("fatal error message: " + e.getMessage());
             responce = "error during addBook procedure";
         }
         return responce;
--- src/main/java/controller/impl/FindNews.java
+++ src/main/java/controller/impl/FindNews.java
@@ -1,26 +1,37 @@
 package controller.impl;
 
-import beans.Category;
-import beans.News;
+
 import controller.Command;
-import service.impl.Converter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import service.NewsService;
 import service.exeption.ServiceException;
 import service.factory.ServiceFactory;
+
 
 /**
  * Created by Mark_Harbunou on 2/1/2017.
  */
 public class FindNews implements Command {
+    final Logger logger = LogManager.getLogger(FindNews.class.getName());
+
     public String execute(String request) {
 
         ServiceFactory serviceFactory = ServiceFactory.getInstance();
         NewsService newsService = serviceFactory.getNewsService();
 
         try {
+            if (logger.isDebugEnabled()) {
+                logger.debug("FindNewsLogger in debug");
+            }
             return newsService.findNews(request);
+
         } catch (ServiceException e) {
+            logger.error("error message: " + e.getMessage());
+            logger.fatal("fatal error message: " + e.getMessage());
             return "News not found";
         }
     }
+
 }
